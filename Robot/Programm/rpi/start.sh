2c1e7616f7b8d36c25a4e0c1b4e45400aae392c9
kvantorium33 <reg@kvantorium33.ru>
2017-08-14 16:37:29 +0300

    1

--- Robot/Programm/rpi/start.sh
+++ Robot/Programm/rpi/start.sh
@@ -2,6 +2,8 @@
 
 pidof  uvc_stream >/dev/null
 if [[ $? -ne 0 ]] ; then
-        echo "Restarting uvc_stream:     $(date)" >> /var/log/kvantorover.log
-        uvc_stream -p 8099 -d /dev/video1 -r 960x720 -f 30 &
+  echo "Restarting uvc_stream: $(date)"
+  uvc_stream -p 8099 -d /dev/video1 -r 960x720 -f 30 &
+else
+  echo  "uvc_stream running"
 fi
