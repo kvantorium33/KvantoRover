6f15c2414f9b25512f0a545c3bcc96baf397992b
kvantorium33 <reg@kvantorium33.ru>
2017-08-14 15:58:03 +0300

    1

--- Robot/Programm/rpi/start.sh
+++ Robot/Programm/rpi/start.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-HOSTNAME=$hostname
-PORT=8099
-DATA=""
-RETRY=15
-
-echo $HOSTNAME
-
-while [ $RETRY -gt 0 ]
-do
-    DATA=$(wget -O - -q -t 1 http://$HOSTNAME:$PORT)
-    if [ $? -eq 0 ]
-    then
-        break
-    else
-        echo "Retrying Again" >&2
-        uvc_stream -p $PORT -d /dev/video0 -r 960x720 -f 30 &
-        let RETRY-=1
-        sleep 30
-    fi
-done
-
-echo "Server is UP"
+pidof  uvc_stream >/dev/null
+if [[ $? -ne 0 ]] ; then
+        echo "Restarting uvc_stream:     $(date)" >> /var/log/kvantorover.log
+        uvc_stream -p 8099 -d /dev/video1 -r 960x720 -f 30 &
+fi
